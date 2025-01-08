8344d4c5cf7f3c2c9c34bb3572e80c24a1a55eee
Ikram <ikramrizq966@gmail.com>
2025-01-08 08:32:09 +0700

    Start with placeholder images

--- pin.js
+++ pin.js
@@ -1,1 +1,18 @@
+const grid = document.querySelector('.masonry');
+const cards = grid.querySelectorAll('.card');
 
+cards.forEach(card => {
+  const img = card.querySelector('img'); 
+  if (!img.complete) {
+    img.onload = () => adjustSpan(card); 
+  } else {
+    adjustSpan(card); 
+  }
+});
+
+function adjustSpan(card) {
+  const rowHeight = 10;
+  const contentHeight = card.getBoundingClientRect().height; 
+  const rowSpan = Math.ceil(contentHeight / rowHeight);
+  card.style.gridRow = `span ${rowSpan}`;
+}
